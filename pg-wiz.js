d3087c4a3a60805dcbad7f39185ad1748a51b6ee
Andrew Chilton <andychilton@gmail.com>
2023-10-22 01:17:46 +1300

    Add functions to help with result manipulation

--- pg-wiz.js
+++ pg-wiz.js
@@ -2,6 +2,7 @@ export class Table {
     constructor(name, prefix) {
         this.name = name
         this.prefix = prefix
+        this.colPrefix = prefix + '__'
         this.cols = []
     }
 
@@ -15,5 +16,27 @@ export class Table {
 
     updCols() {
         return this.cols.map((name, i) => `${name} = $${i+1}`).join(', ')
+    }
+
+    scrubToNewObj(obj) {
+        const newObj = {}
+        for ( const key of Object.keys(obj) ) {
+            if ( key.startsWith(this.colPrefix) ) {
+                newObj[key.substr(this.colPrefix.length)] = obj[key]
+                delete obj[key]
+            }
+        }
+        return newObj
+    }
+
+    scrubToKey(obj) {
+        const newObj = {}
+        for ( const key of Object.keys(obj) ) {
+            if ( key.startsWith(this.colPrefix) ) {
+                newObj[key.substr(this.colPrefix.length)] = obj[key]
+                delete obj[key]
+            }
+        }
+        obj[this.name] = newObj
     }
 }
--- test/001-simple.js
+++ test/001-simple.js
@@ -27,3 +27,33 @@ tap.test('Create a table class', t => {
 
     t.end()
 })
+
+tap.test('Column manipulation', t => {
+    t.plan(4)
+
+    const account = new pgWiz.Table('account', 'acc')
+
+    const date = (new Date()).toISOString()
+    const input = {
+        acc__id: 1,
+        acc__email: 'bob@example.com',
+        acc__logins: 1,
+        acc__inserted: date,
+    }
+    const exp = { id: 1, email: 'bob@example.com', logins: 1, inserted: date }
+
+    const input1 = Object.assign({}, input)
+    const obj1 = account.scrubToNewObj(input1)
+    t.same(obj1, exp, 'Extracted data is correct')
+    t.same(input1, {}, 'Input data is now scrubbed')
+
+    const input2 = Object.assign({}, input)
+    input2.account = account.scrubToNewObj(input2)
+    t.same(input2, { account: exp }, 'Manually extracted data to an object is correct')
+
+    const input3 = Object.assign({}, input)
+    account.scrubToKey(input3)
+    t.same(input3, { account: exp }, 'Extracted data to an object is correct')
+
+    t.end()
+})
